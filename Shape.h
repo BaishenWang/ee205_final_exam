e0a4719dc2a8ed9c57404bd19b11dea6deff6d38
Baishen <baishen@hawaii.edu>
2022-05-13 10:40:53 -1000

    done

---
+++ Shape.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file Shape.h
+/// @version 1.0
+///
+/// @author Baishen Wang <baishen@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef FINAL_EXAM_SHAPE_H
+#define FINAL_EXAM_SHAPE_H
+
+class Shape {
+public:
+    virtual double compute_area()=0;
+};
+
+#endif //FINAL_EXAM_SHAPE_H
